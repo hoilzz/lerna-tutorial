8ce68f48643f794826a40f4758c797bfc43e15f5
hoilzz <qqwq9999@gmail.com>
2021-09-06 15:27:38 +0900

    sdfvsdv

--- packages/log-core/lib/log-core.js
+++ packages/log-core/lib/log-core.js
@@ -1,9 +1,11 @@
 'use strict';
 
 const { red } = require('chalk');
+const { print } = require('log-util');
 
 module.exports = logCore;
 
 function logCore() {
+  console.log(print());
   console.log(red('🚀 Running Core!'));
 }
---
+++ packages/util/lib/util.js
@@ -0,0 +1,10 @@
+'use strict';
+
+module.exports = {
+  print,
+};
+
+function print() {
+  console.log('this is util!');
+  // TODO
+}
